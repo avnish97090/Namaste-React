7b0a6e28707ee617a833c2e50bb9a95694bee4b9
ABANISH <avnish97090@gmail.com>
2024-01-05 18:03:22 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,41 @@
+// const heading =  React.createElement("h1", { id : "heading2" , xyz :"abc"} , "Hello world from React! ")   
+
+
+
+
+// <div id ="parent">
+//      <div id = "child">
+//         <h1> I'm h1 tag </h1>
+        //   <h2> This is sibling </h2>
+//      </div>
+
+// <div id = "child">
+//          <h1> I'm h1 tag </h1>
+//        <h2> This is sibling </h2>
+//       </div>
+
+// </div>
+
+
+
+const parent = React.createElement("div" , {id :"parent" },[
+React.createElement(
+    "div" , {id : "child"}, 
+    [
+    React.createElement("h1", {} , " I'm an h1 tag"),
+    React.createElement("h2" , {} , "This is sibling")
+
+    ]),
+    React.createElement(
+        "div" , {id : "child"}, 
+        [
+        React.createElement("h1", {} , " I'm an h1 tag"),
+        React.createElement("h2" , {} , "This is sibling")
+    
+        ]),
+    ]
+);
+
+const root = ReactDOM.createRoot (document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
