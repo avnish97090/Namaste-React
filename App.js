6257cc8ce52026d9b573ec5b94829afabcfad9da
ABANISH <avnish97090@gmail.com>
2024-01-07 16:26:35 +0530

    third episode

--- App.js
+++ App.js
@@ -39,4 +39,42 @@ React.createElement(
 
 const root = ReactDOM.createRoot (document.getElementById("root"));
 // root.render(heading);
-root.render(parent);
+// root.render(parent);
+
+
+
+const jsxfile = <h1 id = "heading" className="head"> its is a jsx file </h1>
+
+
+const jsxfile2 = (<h1 id = "heading2" className="head1"> 
+its is a jsx file2 </h1>);  // for mutiple line we wrap up jsx into ()
+
+// root.render(jsxfile2);
+
+
+
+const Headingcomponent = () => {
+   return  <div id = " container">
+   <h1> Namaste React Functional component 1 </h1>
+   <Headingcomponent2/>
+   <Headingcomponent3/>
+   </div>
+};
+
+const Headingcomponent2 = () => (
+    <div id = "container ">
+<h1>Namaste react Functional component 2</h1>
+<Headingcomponent3/>
+</div>
+);
+
+
+const Headingcomponent3 = () => (
+    <div id = "container ">
+    <h1 className="heading">Namaste React Component 3 </h1>
+    
+    </div>
+);   // for mutiple line we wrap up with ()
+
+
+root.render(<Headingcomponent/>);
