cfc8ecbb29e659fd6d8a5e3cb40201d9d621b50d
ABANISH <avnish97090@gmail.com>
2024-01-11 07:17:36 +0530

    fifth episode

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,31 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    const {
+      cloudinaryImageId,
+      name,
+      cuisines,
+      avgRating,
+      sla: { deliveryTime },
+    } = resData?.info;
+    return (
+      <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+        <img
+          className="res-logo"
+          alt="res-logo"
+          src={CDN_URL
+            +
+            resData.info.cloudinaryImageId
+          }
+        />
+             
+        <h3>{name}</h3>
+        <h5>{cuisines.join(" , ")}</h5>
+        <h5>{avgRating} stars</h5>
+        <h5>{resData.info.sla.deliveryTime} minutes</h5>
+      </div>
+    );
+  };
+
+  export default RestaurantCard;
