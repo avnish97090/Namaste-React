baf480abb7093b0b94c9e632f3ea54667f515515
ABANISH <avnish97090@gmail.com>
2024-01-13 23:44:03 +0530

    6th episode till login

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,41 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 const Body = () => {
 
-    const [listOfRestaurants , setListOfRestaurant] = useState(resList);
-    return (
+    const [listOfRestaurants , setListOfRestaurant] = useState([]);
+    const[searchText,setsearchText] = useState("");
+
+   useEffect (()=> {
+     fetchData();
+   },[]);
+        
+   const fetchData = async () => {
+    const data = await fetch (
+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7076743&lng=77.0972752&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    
+   };
+      
+  //  if(listOfRestaurants.length === 0){   conditional  rendering 
+  //   return <Shimmer/> 
+  //  }
+     return listOfRestaurants.length===0 ?<Shimmer/> :
+        (
+
       <div className="body">
+       
         <div className="filter">
+        <div className="search"> 
+        <input type="text" className="search-box" value={searchText}/>
+        <button onClick={()=> {
+
+        }}>Search</button>
+        </div>
             <button className="filter-btn" onClick={ () =>{
                 const filteredList = listOfRestaurants.filter(
                     (res) => res.info.avgRating > 4
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
-
+import { useState } from "react";
 const Header = () => {
+  const [btnNameReact , setbtnNameReact] = useState("Login");  
+
     return (
       <div className="header">
         <div className="logo-container">
@@ -15,6 +17,15 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button className="login"
+             onClick={() => {
+                 btnNameReact === "Login" ?
+                setbtnNameReact("Logout") :
+                setbtnNameReact("Login");
+            }}
+            >
+            {btnNameReact} 
+            </button>
           </ul>
         </div>
       </div>
@@ -22,4 +33,3 @@ const Header = () => {
   };
 
   export default Header ;
-  
