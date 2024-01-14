6a9af2ec78418b434621c71faddbb397a75758d8
ABANISH <avnish97090@gmail.com>
2024-01-14 16:46:58 +0530

    6th episode complete

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,59 +3,77 @@ import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 import Shimmer from "./Shimmer";
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setsearchText] = useState("");
+  
 
-    const [listOfRestaurants , setListOfRestaurant] = useState([]);
-    const[searchText,setsearchText] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-   useEffect (()=> {
-     fetchData();
-   },[]);
-        
-   const fetchData = async () => {
-    const data = await fetch (
-"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7076743&lng=77.0972752&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7076743&lng=77.0972752&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
-    setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
-    
-   };
-      
-  //  if(listOfRestaurants.length === 0){   conditional  rendering 
-  //   return <Shimmer/> 
-  //  }
-     return listOfRestaurants.length===0 ?<Shimmer/> :
-        (
-
-      <div className="body">
-       
-        <div className="filter">
-        <div className="search"> 
-        <input type="text" className="search-box" value={searchText}/>
-        <button onClick={()=> {
-
-        }}>Search</button>
-        </div>
-            <button className="filter-btn" onClick={ () =>{
-                const filteredList = listOfRestaurants.filter(
-                    (res) => res.info.avgRating > 4
-                );
-                setListOfRestaurant(filteredList);
-                }
-                }>Top Rated Restaurants</button>
-
-
-
-        </div>
-        <div className="res-container">
-        {
-          listOfRestaurants.map( (restaurant) => 
-          <RestaurantCard  key={restaurant.info.id}  resData = {restaurant}/>)
-        }
-        </div>
-      </div>
+    setListOfRestaurant(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
+    setFilteredRestaurant( json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
   };
 
 
-  export default Body ;
+
+
+  //  if(listOfRestaurants.length === 0){   conditional  rendering
+  //   return <Shimmer/>
+  //  }
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
