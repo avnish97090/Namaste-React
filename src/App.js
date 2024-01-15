d121c3852cf6a4daa411b739d119bb43240c7fd9
ABANISH <avnish97090@gmail.com>
2024-01-15 21:12:22 +0530

    episode 7 part4

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Header from "./components/Header.js";
 import Body from "./components/Body.js";
 import About from "./components/About.js";
 import Contact from "./components/Contact.js";
-import { createBrowserRouter,RouterProvider} from "react-router-dom";
 import Error from "./components/Error.js";
+import { createBrowserRouter,RouterProvider, Outlet} from "react-router-dom";
+
 
 
 
@@ -13,7 +14,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet/>
     </div>
   );
 };
@@ -22,16 +23,23 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout/>,
+    children: [
+      {
+        path: "/",
+        element: <Body/>,
+      },
+      {
+        path: "/about",
+        element:<About/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      },
+    ],
     errorElement: <Error/>
   },
-  {
-    path: "/about",
-    element:<About/>,
-  },
-  {
-    path: "/contact",
-    element: <Contact/>,
-  },
+
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
