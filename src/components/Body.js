d4708ee82fbe2da81a05a8af5bdb2e346856bc86
ABANISH <avnish97090@gmail.com>
2024-01-17 22:19:27 +0530

    online offline feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState([]);
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
@@ -24,6 +25,15 @@ const Body = () => {
     );
     setFilteredRestaurant( json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
   };
+
+const onlineStatus = useOnlineStatus();
+
+if(onlineStatus === false ) 
+return (
+<h1>
+  Looks like you're offline!! Please check your internet connection 
+  </h1>
+);
 
 
 
