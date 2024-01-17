fd43ea4f366f7f6a9f43ae019e7174f11f50e515
ABANISH <avnish97090@gmail.com>
2024-01-17 20:40:09 +0530

    custom hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,25 +1,29 @@
-import {useState, useEffect } from "react";
+// import {useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-// import { MENU_API } from "../utils/constants";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+
+
 const RestaurantMenu = () =>{
 
-    const [resInfo , setresInfo] = useState(null);
+    // const [resInfo , setresInfo] = useState(null); 
 
     const {resId} = useParams(); 
 
- useEffect(()=>{
-  fetchMenu();
- },[]);
+    const resInfo = useRestaurantMenu(resId);
+
+//  useEffect(()=>{
+//   fetchMenu();
+//  },[]);
 
 
- const fetchMenu = async () => {
-     const data =  await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7076743&lng=77.0972752&restaurantId="+ resId+ "&catalog_qa=undefined&submitAction=ENTER");
-     const json =await  data.json() ;
-    //  console.log(json);  
+//  const fetchMenu = async () => {
+//      const data =  await fetch (MENU_API+resId+"&catalog_qa=undefined&submitAction=ENTER");
+//      const json =await  data.json() ;
+//     //  console.log(json);  
 
-     setresInfo(json.data);
- }
+//      setresInfo(json.data);
+//  }
 
  if (resInfo===null) return<Shimmer/> ;
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,6 @@ export const LOGO_URL = "https://i.pinimg.com/originals/82/ba/7d/82ba7db3760b88d
 
 
 // export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7076743&lng=77.0972752&restaurantId=" ;
+
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7076743&lng=77.0972752&restaurantId=";
