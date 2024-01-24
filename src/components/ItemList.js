1e6fd363e2ce2a365129820b5e13075daafd3cc3
ABANISH <avnish97090@gmail.com>
2024-01-24 16:17:44 +0530

    11th telwind part finish till 1.26

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,13 @@
+import { CDN_URL } from "../utils/constants";
+
 const ItemList = ({items}) => {
     console.log(items);
     return (<div>
         {items.map(item => (
         <div key = {item?.card?.info?.id || item?.title}
-        className="p-2 m-2 border-gray-300 border-b-2 text-left">
+        className="p-2 m-2 border-gray-300 border-b-2 text-left flex justify-between">
+
+            <div className="w-9/12">
             <div className="py=2">
                 <span>{item?.card?.info?.name || item?.title}</span>
                 <span>
@@ -12,6 +16,13 @@ const ItemList = ({items}) => {
                 : item.card?.info?.defaultPrice /100}</span>
         </div>
         <p className="text-xs">{item?.card?.info?.description}</p>
+        </div>
+        <div className="w-3/12 p-4">
+            <div className="absolute">
+            <button className="p-2 mx-16 rounded-lg bg-white text-green-600 shadow-lg">Add +</button>
+              </div>
+        <img src= {CDN_URL + item?.card?.info?.imageId} className="w-full"/>
+         </div>
         </div>
         ))}
         </div>
